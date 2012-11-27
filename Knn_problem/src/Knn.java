a4ca2f1a6f3e724454a3c424845fb6d518c064bd
Tanmay Mathur <tanmaymathur89@gmail.com>
2012-11-27 15:18:44 -0500

    commit

--- Knn_problem/src/Knn.java
+++ Knn_problem/src/Knn.java
@@ -12,7 +12,10 @@ import java.util.Comparator;
 
 public class Knn {
 	
-	static final int k = 3; //Set the value of k here
+	static final int k = 5; //Set the value of k here
+	static int correct = 0;
+	static int wrong = 0;
+	
 	ArrayList<Integer> distance = new ArrayList<Integer>();
 	
 	/**
@@ -21,11 +24,15 @@ public class Knn {
 	public static void main(String[] args) {
 		
 		ArrayList<Attributes> instances = new ArrayList<Attributes>();
-		
 		instances = FileReader.readFile();
 		/*for(Attributes attr1 : instances)
 		System.out.println(attr1);*/
 		cross_validation(instances);
+		//double Percent = (correct/(correct + wrong))*100;
+		double Percent = 0.0;
+		Percent = (correct/345.0)*100;
+		System.out.println("Percentage of correct predictions " + Percent + "%");
+		System.out.println("Percentage of wrong predictions " + (100 - Percent) + "%");
 		
 	}
 	
@@ -82,9 +89,22 @@ public class Knn {
 				count_2++;
 		}
 		if(count_1 > count_2)
-			System.out.println("Prediction based on " + k + " nearest neighbour "+ "1.0" + " Original " + atr.selector);
+			{
+				System.out.println("Prediction based on " + k + " nearest neighbour "+ "1.0" + " Original " + atr.selector);
+				if(atr.selector == 1.0)
+					correct++;
+				else
+					wrong++;
+			}
 		else
+		{
 			System.out.println("Prediction based on " + k + " nearest neighbour " + "2.0" + " Original " + atr.selector);
+			if(atr.selector == 2.0)
+				correct++;
+			else
+				wrong++;
+		}
+			
 	}
 
 	/**
